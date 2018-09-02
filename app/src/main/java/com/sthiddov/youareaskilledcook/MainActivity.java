25a364c29fc4e13fc74242fd3ab4e3476ed1c550
kmm <fike2098@gmail.com>
2018-09-02 10:02:49 +0100

    123

--- app/src/main/java/com/sthiddov/youareaskilledcook/MainActivity.java
+++ app/src/main/java/com/sthiddov/youareaskilledcook/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent i=new Intent(this,splash.class);
-        startActivity(i);
+      /*  Intent i=new Intent(this,splash.class);
+        startActivity(i);*/
     }
 }
